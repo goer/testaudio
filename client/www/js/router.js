deeb7d731b5bebaf01528fb7066055e30ce3c0a8
goer <fonetix@gmail.com>
2015-05-07 14:42:33 +0700

    working on routing & workflow 2nd

--- client/www/js/router.js
+++ client/www/js/router.js
@@ -80,6 +80,91 @@ angular.module('Router',['Auth'])
 
             })
 
+            .state('settings', {
+                url: "/settings",
+                views: {
+                    'main': {
+                        templateUrl: "tpl/settings.html",
+                        controller: 'MainCtrl',
+                    }
+                },
+                data: {
+                    requireLogin: true
+                }
+
+            })
+
+            .state('users', {
+                url: "/users",
+                views: {
+                    'main': {
+                        templateUrl: "tpl/users.html",
+                        controller: 'MainCtrl',
+                    }
+                },
+                data: {
+                    requireLogin: true
+                }
+
+            })
+
+            .state('adduser', {
+                url: "/adduser",
+                views: {
+                    'main': {
+                        templateUrl: "tpl/adduser.html",
+                        controller: 'MainCtrl',
+                    }
+                },
+                data: {
+                    requireLogin: true
+                }
+
+            })
+
+            .state('userdetail', {
+                url: "/userdetail",
+                views: {
+                    'main': {
+                        templateUrl: "tpl/userdetail.html",
+                        controller: 'MainCtrl',
+                    }
+                },
+                data: {
+                    requireLogin: true
+                }
+
+            })
+
+            .state('members', {
+                url: "/members",
+                views: {
+                    'main': {
+                        templateUrl: "tpl/members.html",
+                        controller: 'MainCtrl',
+                    }
+                },
+                data: {
+                    requireLogin: true
+                }
+
+            })
+
+            .state('memberdetail', {
+                url: "/memberdetail",
+                views: {
+                    'main': {
+                        templateUrl: "tpl/memberdetail.html",
+                        controller: 'MainCtrl',
+                    }
+                },
+                data: {
+                    requireLogin: true
+                }
+
+            })
+
+
         $urlRouterProvider.otherwise('/login');
 
     })
